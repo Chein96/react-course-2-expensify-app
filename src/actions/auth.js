817ef05a938a60b459d6d848ca7ab336d543d13b
Alex Chein <miguelalexis@hotmail.com>
2020-07-21 16:43:00 -0700

    Added facebook login functionality

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,15 +1,21 @@
-import { firebase, googleAuthProvider } from '../firebase/firebase';
+import { firebase, googleAuthProvider, facebookAuthProvider } from '../firebase/firebase';
 
 export const login = (uid) => ({
     type: 'LOGIN',
     uid
 });
 
-export const startLogin = () => {
+export const startGoogleLogin = () => {
     return () => {
         return firebase.auth().signInWithPopup(googleAuthProvider);
     }
 };
+
+export const startFacebookLogin = () => {
+    return () => {
+        return firebase.auth().signInWithPopup(facebookAuthProvider);
+    }
+}
 
 export const logout = () => ({
     type: 'LOGOUT'
--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -2,14 +2,28 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { LoginPage } from '../../components/LoginPage';
 
+let startGoogleLogin, startFacebookLogin, wrapper;
+beforeEach(() => {
+    startGoogleLogin = jest.fn();
+    startFacebookLogin = jest.fn();
+    wrapper = shallow(
+        <LoginPage
+            startGoogleLogin={startGoogleLogin}
+            startFacebookLogin={startFacebookLogin}
+        />
+    );
+});
+
 test('Should render LoginPage correctly', () => {
-    const wrapper = shallow(<LoginPage startLogin={() => {}} />);
     expect(wrapper).toMatchSnapshot();
 });
 
-test('Should call startLogin on button click', () => {
-    const startLogin = jest.fn();
-    const wrapper = shallow(<LoginPage startLogin={startLogin} />);
-    wrapper.find('button').simulate('click');
-    expect(startLogin).toHaveBeenCalled();
+test('Should call startGoogleLogin on button click', () => {
+    wrapper.find('button').at(0).simulate('click');
+    expect(startGoogleLogin).toHaveBeenCalled();
+});
+
+test('Should call startFacebookLogin on button click', () => {
+    wrapper.find('button').at(1).simulate('click');
+    expect(startFacebookLogin).toHaveBeenCalled();
 });
